b621c7149fad15ff77e0aa17a03fbcc4f07f819e
Anand Vijayan <anand.vijayan@outlook.com>
2024-12-31 13:16:42 +0530

    Completed few more

--- JavaLab/src/main/java/org/dto/UnionFind.java
+++ JavaLab/src/main/java/org/dto/UnionFind.java
@@ -5,7 +5,13 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class UnionFind {
-    private Map<String, String> parent = new LinkedHashMap<>();
+    public Map<String, String> parent;
+    public Map<String, Double> weight;
+
+    public UnionFind() {
+        parent = new HashMap<>();
+        weight = new HashMap<>();
+    }
 
     public String find(String s) {
         if (!parent.containsKey(s)) parent.put(s, s);
@@ -17,5 +23,24 @@ public class UnionFind {
         String root1 = find(s1);
         String root2 = find(s2);
         if (!root1.equals(root2)) parent.put(root1, root2);
+    }
+
+    // Union two variables
+    public void union(String x, String y, double value) {
+        String rootX = find(x);
+        String rootY = find(y);
+
+        if (!rootX.equals(rootY)) {
+            parent.put(rootX, rootY);
+            weight.put(rootX, value * weight.get(y) / weight.get(x));
+        }
+    }
+
+    // Add a variable and initialize its parent and weight
+    public void add(String x) {
+        if (!parent.containsKey(x)) {
+            parent.put(x, x);
+            weight.put(x, 1.0);
+        }
     }
 }
--- JavaLab/src/main/java/org/heycoach/Main.java
+++ JavaLab/src/main/java/org/heycoach/Main.java
@@ -6,12 +6,19 @@ import static org.modules.AdvancedDataStructure.*;
 public class Main {
     public static void main(String[] args)  {
 
-        int[] input = new int[] {5,15,1,3};
+        // Example 1
+        int N = 6, E = 7;
+        int[][] edges = new int[][] {{0, 1},{0, 2},{1, 2},{1, 3},{2, 4},{3, 4},{3, 5}};
+        int S = 0, D = 5;
 
-        for(int a : input) {
-            System.out.print(MedianAtEveryStep(a) + " ");
-        }
-        System.out.println();
+        System.out.println(shortestPath(N, edges, S, D));  // Output: 3
+
+        // Example 2
+        N = 5; E = 5;
+        edges = new int[][] {{0, 1}, {1, 2}, {1, 3}, {2, 3}, {3, 4}};
+        S = 0; D = 4;
+
+        System.out.println(shortestPath(N, edges, S, D));  // Output: 3
     }
 }
 
